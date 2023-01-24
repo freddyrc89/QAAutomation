9652b5ff127ed2724696744cd81d9073ff933b3b
fredd <fredd@LAPTOP-1RIICR7F>
2023-01-24 13:21:23 -0500

    POM

---
+++ src/test/java/com/project/pom/SignIn_Test.java
@@ -0,0 +1,35 @@
+package com.project.pom;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignIn_Test {
+
+	private WebDriver driver;
+	SignInPage signInPage;
+	@Before
+	public void setUp() throws Exception {
+		signInPage = new SignInPage(driver);
+		driver = signInPage.chromeDriverConnection();
+		signInPage.visit("https://demo.guru99.com/test/newtours/");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//driver.quit();
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		signInPage.signIn();
+		Thread.sleep(2000);
+		assertEquals("Login Successfully",driver.findElement(By.tagName("h3")).getText());
+		//assertTrue(signInPage.isHomePageDisplayed());
+	}
+
+}
--- src/test/java/com/qualitystream/tutorial/GoogleSearchTest.java
+++ src/test/java/com/qualitystream/tutorial/GoogleSearchTest.java
@@ -1,20 +1,29 @@
 package com.qualitystream.tutorial;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GoogleSearchTest 
 {
 	private WebDriver driver;
+	By videoLinkLocator = By.cssSelector("a[href='https://www.ibm.com/es-es/topics/software-testing']");
 	
 	@Before
 	public void setUp()
@@ -33,9 +42,35 @@ public class GoogleSearchTest
 		searchbox.clear();
 		searchbox.sendKeys("pruebas de software");
 		searchbox.submit();
-		driver.manage().timeouts().implicitlyWait(100,TimeUnit.SECONDS);
 		
-		assertEquals("pruebas de software - Buscar con Google", driver.getTitle());
+		//Implicit wait
+		//driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		
+		//Explicit wait
+		//WebDriverWait ewait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		//ewait.until(ExpectedConditions.titleContains("pruebas de software"));
+		
+		//assertEquals("pruebas de software - Buscar con Google", driver.getTitle());
+		
+		//FluentWait
+		Wait<WebDriver> fwait = new FluentWait<WebDriver>(driver)
+				.withTimeout(Duration.ofSeconds(10))
+				.pollingEvery(Duration.ofSeconds(2))
+				.ignoring(NoSuchElementException.class);
+		
+//		WebElement video = fwait.until(new Function<WebDriver,WebElement>(){
+//			public WebElement apply(WebDriver driver) {
+//				return driver.findElement(videoLinkLocator);
+//			}
+//		})
+		   WebElement video = fwait.until(new Function<WebDriver,WebElement>()
+				   {
+			   public WebElement apply(WebDriver driver) {
+				   return driver.findElement(videoLinkLocator);
+			   }
+				   }
+				   );
+		   assertTrue(driver.findElement(videoLinkLocator).isDisplayed());
 	}
 	
 	@After
